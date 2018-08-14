635b1bfdd459e4114caf973e317dc8fee1b8f390
Nicolas Bétheuil <nicolas.betheuil@mappy.com>
2018-08-14 18:25:08 +0200

    add hash params

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import Router from "universal-router"
+import queryString from 'querystring'
 
 export default (app) => {
-    return new Router([
+    return new Router({
+        action(context) {
+            context.params = Object.assign(context.params, queryString.parse(context.hash.slice(1)))
+        },
+        children : [
         { name: 'geoentity', path: '/geoentity/:id', action: (context, params) => app.showGeoentity(params) },
         { path: '/', action: () => app.home()},
         { name: 'addresses', path: '/addresses', action: () => app.search() },
-    ])
+    ]})
 }
